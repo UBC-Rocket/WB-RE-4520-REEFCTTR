7c79a1557f18f209292d740734060dda4382badd
Hitesh <108963327+UrAvgStudent@users.noreply.github.com>
2022-11-20 14:40:19 -0800

    printf

--- WBRecovery.c
+++ WBRecovery.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello world!");
+    int x = hitesh(31);
+    printf(x);
+    return 0;
+}
+
+int hitesh(int number) {
+    printf("hello conner");
+    for (int i = 2; i < number; i++) {
+        if (number % i == 0){
+            return 1;
+        }
+    }
+    
     return 0;
 }
