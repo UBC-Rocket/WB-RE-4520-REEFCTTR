1c6a5ee416bbbc2b2baf30bc56375442e60ce76a
Sabrina <sabrinapstarfire@gmail.com>
2022-11-20 14:42:35 -0800

    created function sabrina

--- WBRecovery.c
+++ WBRecovery.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
+int sabrina(int num);
+
 int main() {
     printf("Hello world!");
+
+     int x=sabrina(32);
+    printf("%d\n", x);
+    
     return 0;
 }
+
+int sabrina(int num){
+    for(int i=2; i<num; i++){
+        if(num % i == 0){
+            return 0;
+        }
+        else{
+            return 1;
+        }
+    }
+}
+
