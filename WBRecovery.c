d52004535a07403659dfca75079e2d20f3b87e14
Dinglebergle <dingk3608@gmail.com>
2022-11-20 14:48:39 -0800

    primeNumberChecker

--- WBRecovery.c
+++ WBRecovery.c
@@ -2,5 +2,32 @@
 
 int main() {
     printf("Hello world!");
+    kevin(1)
+    printf(isTrue)
     return 0;
+}
+
+int i;
+int isTrue;
+int kevin(int num) 
+{
+    for i =2;i < num ; ++1
+    {
+        if isTrue == 0 {
+            if i == num
+               {
+                 isTrue = 1
+                    return (isTrue = 0)
+                }
+             else if num%i == 0 
+                {
+                    return (isTrue = 1);
+                }
+        
+        }
+       else 
+       {
+        return 0;
+       }
+    }   
 }
